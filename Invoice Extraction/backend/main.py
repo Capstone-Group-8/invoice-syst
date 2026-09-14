34a19d609c468c8d5cf3f8895dd2545a99eed10d
PlanetaryOS <83846077+PlanetaryOS@users.noreply.github.com>
2026-09-14 14:59:52 -0700

    Update main.py
     lines 9-10 into lines 9-12 to work with other systems

--- Invoice Extraction/backend/main.py
+++ Invoice Extraction/backend/main.py
@@ -6,8 +6,10 @@ from invoice_parser import parse_invoice_fields
 import shutil
 import use_data
 
-DROPBOX = "C:/HW/invoice extractor/dropbox"
-PROCESSED = "C:/HW/invoice extractor/processed"
+DROPBOX = os.path.join(BASE_DIR, "dropbox")
+PROCESSED = os.path.join(BASE_DIR, "processed")
+os.makedirs(DROPBOX, exist_ok=True)
+os.makedirs(PROCESSED, exist_ok=True)
 
 os.makedirs(DROPBOX, exist_ok=True)
 os.makedirs(PROCESSED, exist_ok=True)
